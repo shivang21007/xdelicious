9dfadd083b8e7eccee23e2700dac44caab5807bc
Shivang21007 <shivanggupta2611@gmail.com>
2023-08-17 13:49:05 +0530

    gitignore modified

---
+++ src/apikey.js
@@ -0,0 +1,12 @@
+
+///require('dotenv').config();
+
+const API_KEY = '7004ee6312344b348cf7a8bd50435420'
+
+// const API_KEY = `${process.env.secret}`; 
+// //console.log(API_KEY);
+
+export {API_KEY};
+
+
+
